0a125cffa77011fc497240c9a6e2a44e7172f516
Simon Hawkenson <7358924+sihawken@users.noreply.github.com>
2023-08-26 16:57:08 -0400

    gnome shell extension installer

---
+++ scripts/post/example.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Your code goes here.
+wget -O gnome-shell-extension-installer "https://github.com/brunelli/gnome-shell-extension-install"
+chmod +x gnome-shell-extension-installer
+mv gnome-shell-extension-installer /usr/bin/
