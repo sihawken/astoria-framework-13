fc0b02d306fec916f9f09d4398905ba7a8422f8e
Simon Hawkenson <7358924+sihawken@users.noreply.github.com>
2023-08-27 11:29:59 -0400

    bypass pipefail for now

--- scripts/pre/dconf_update.sh
+++ scripts/pre/dconf_update.sh
@@ -3,7 +3,7 @@
 # Tell this script to exit if there are any errors.
 # You should have this in every custom script, to ensure that your completed
 # builds actually ran successfully without any errors!
-set -oue pipefail
+#set -oue pipefail
 
 systemctl unmask dconf-update.service
 systemctl enable dconf-update.service
