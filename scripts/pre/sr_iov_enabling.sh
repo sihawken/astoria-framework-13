a6a425a59ac3b8595b2748ee55822b032d2ba86d
Simon Hawkenson <7358924+sihawken@users.noreply.github.com>
2023-08-29 11:42:18 -0230

    Delete original i915 kernel module

--- scripts/pre/sr_iov_enabling.sh
+++ scripts/pre/sr_iov_enabling.sh
@@ -10,6 +10,9 @@ rpm-ostree install sysfsutils
 
 echo "devices/pci0000:00/0000:00:02.0/sriov_numvfs = 7" > /etc/sysfs.conf
 
+# Delete other kernel module
+rm -rf /usr/lib/modules/"$(rpm -qa kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"/kernel/drivers/gpu/drm/i915/i915.ko.xz
+
 # allow simple `dnf install` style commands to work (in some spec scripts)
 ln -s /usr/bin/rpm-ostree /usr/bin/dnf
 ln -s /usr/bin/ld.bfd /etc/alternatives/ld && ln -s /etc/alternatives/ld /usr/bin/ld 
