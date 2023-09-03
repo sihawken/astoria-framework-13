b1ff4a3d11ea70c956e9bb2c4ce9907d90552921
Simon Hawkenson <7358924+sihawken@users.noreply.github.com>
2023-09-03 19:25:22 -0400

    Add virtualization packages

---
+++ scripts/post/kvm.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+rpm-ostree install virt-install libvirt-daemon-config-network libvirt-daemon-kvm qemu-kvm \
+    virt-manager virt-viewer guestfs-tools python3-libguestfs virt-top
