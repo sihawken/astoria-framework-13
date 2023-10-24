cbfcaed4c80068d47737d3dea66053777a97d006
Simon Hawkenson <7358924+sihawken@users.noreply.github.com>
2023-10-24 14:42:33 -0230

    remove sysfsutils, reset the device id

--- scripts/pre/kernel.sh
+++ scripts/pre/kernel.sh
@@ -6,10 +6,6 @@
 
 set -oue pipefail
 
-rpm-ostree install sysfsutils
-
-echo "devices/pci0000:00/0000:00:02.0/sriov_numvfs = 1" > /etc/sysfs.conf
-
 # allow simple `dnf install` style commands to work (in some spec scripts)
 ln -s /usr/bin/rpm-ostree /usr/bin/dnf
 ln -s /usr/bin/ld.bfd /etc/alternatives/ld && ln -s /etc/alternatives/ld /usr/bin/ld 
@@ -23,5 +19,5 @@ depmod -a "$(rpm -qa kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 # Regenerate the initramfs
 /usr/bin/dracut --reproducible -v --add 'ostree' --no-hostonly -f /lib/modules/"$(rpm -qa kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"/initramfs.img "$(rpm -qa kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 
-echo 'ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x8086", ATTR{device}=="0x9a60", ATTR{sriov_numvfs}="1"' >  /etc/udev/rules.d/5-sriov.rules
+echo 'ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x8086", ATTR{device}=="0x4626", ATTR{sriov_numvfs}="1"' >  /etc/udev/rules.d/5-sriov.rules
 
