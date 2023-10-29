bb97d7fd01d5f8ac97840577daa65d5ef39c2322
Simon Hawkenson <7358924+sihawken@users.noreply.github.com>
2023-10-29 13:26:55 -0230

    disable new SRIOV i915 module

--- scripts/pre/2-kernel.sh
+++ scripts/pre/2-kernel.sh
@@ -11,13 +11,13 @@ ln -s /usr/bin/rpm-ostree /usr/bin/dnf
 ln -s /usr/bin/ld.bfd /etc/alternatives/ld && ln -s /etc/alternatives/ld /usr/bin/ld 
 
 # Install akmod-i915
-rpm-ostree install akmod-i915-sriov
-akmods --force --kernels "$(rpm -qa kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
+# rpm-ostree install akmod-i915-sriov
+# akmods --force --kernels "$(rpm -qa kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 
-depmod -a "$(rpm -qa kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
+# depmod -a "$(rpm -qa kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 
 # Regenerate the initramfs
-/usr/bin/dracut --reproducible -v --add 'ostree' --no-hostonly -f /lib/modules/"$(rpm -qa kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"/initramfs.img "$(rpm -qa kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
+# /usr/bin/dracut --reproducible -v --add 'ostree' --no-hostonly -f /lib/modules/"$(rpm -qa kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"/initramfs.img "$(rpm -qa kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 
-echo 'ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x8086", ATTR{device}=="0x4626", ATTR{sriov_numvfs}="1"' >  /etc/udev/rules.d/5-sriov.rules
+# echo 'ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x8086", ATTR{device}=="0x4626", ATTR{sriov_numvfs}="1"' >  /etc/udev/rules.d/5-sriov.rules
 
