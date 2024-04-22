f4f1027ea30fccc9589ee2b20cce572e4347b713
Simon Hawkenson <7358924+sihawken@users.noreply.github.com>
2024-04-22 12:20:57 -0230

    Remove incompatible gnome extensions

--- scripts/post/patched_gnome_mutter.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Tell this script to exit if there are any errors.
-# You should have this in every custom script, to ensure that your completed
-# builds actually ran successfully without any errors!
-set -oue pipefail
-
-# Provides Mutter & GNOME Control Center with Dor Askayo's Wayland VRR MR applied.
-rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:kylegospo:gnome-vrr mutter mutter-common gnome-control-center gnome-control-center-filesystem xorg-x11-server-Xwayland
